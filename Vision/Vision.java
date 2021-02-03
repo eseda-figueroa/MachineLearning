451bd8f3edf8555cf0b18720d6e0d6d97b04ed65
Elian Seda-Figueroa <elian.seda@gmail.com>
2021-02-03 17:31:45 -0500

    Project Start

---
+++ Vision/Vision.java
@@ -0,0 +1,21 @@
+import com.google.cloud.vision.v1.AnnotateImageRequest;
+import com.google.cloud.vision.v1.AnnotateImageResponse;
+import com.google.cloud.vision.v1.BatchAnnotateImagesResponse;
+import com.google.cloud.vision.v1.EntityAnnotation;
+import com.google.cloud.vision.v1.Feature;
+import com.google.cloud.vision.v1.Image;
+import com.google.cloud.vision.v1.ImageAnnotatorClient;
+import com.google.protobuf.ByteString;
+import java.io.*;
+import java.util.*;
+
+
+
+public class Vision
+{
+    public static void main(String[] args)
+    {
+        String path = "/Vision/receipt.jpg";
+        ArrayList<AnnotateImageRequest> request = new ArrayList<>();
+    }
+}
